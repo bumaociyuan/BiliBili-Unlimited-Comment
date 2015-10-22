a63dae64720558865c5f8d7fe6f9fe468441fb09
ZX <bumaociyuan@users.noreply.github.com>
2015-10-22 09:34:14 +0800

    Update BiliBili-Unlimited-Comment.js

--- BiliBili-Unlimited-Comment.js
+++ BiliBili-Unlimited-Comment.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibli 发弹幕无限制
 // @namespace    http://bumaociyuan.github.io/
-// @version      0.1
+// @version      0.2
 // @description  enter something useful
 // @author       bumaociyuan
 // @match        http://www.bilibili.com/video/*
@@ -11,7 +11,7 @@
 
 (function($) {
 
-    var t = window.setInterval(function() { //百度云把一些内容放到后面加载,因此我设置了一个延时循环，每隔100ms选择一下所需的元素，当所需的元素存在时，开始脚本，同时停止延时循环
+    var t = window.setInterval(function() { 
         if ($(".ABP-Comment-Input").length > 0) {
             window.clearInterval(t);
             console.log('done');
